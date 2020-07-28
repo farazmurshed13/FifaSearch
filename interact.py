93de3cd407a6cf1b8f0a4e77896cbc819218dba1
Faraz Murshed <farazmurshed@gmail.com>
2020-07-28 15:35:04 -0700

    implemented search functionality and unicode conversions

--- interact.py
+++ interact.py
@@ -1,8 +1,31 @@
 import soFifaScraper as sFS
-import pandas as pd
+import json as js
 
+# get requested player data
 bank = sFS.get_player_data()
+with open(bank, 'r') as f:
+    data = js.load(f)
 
-df = pd.read_json(open(bank, 'r'))
-df.set_index('name', inplace=True)
-print(df.head())
+# prompt user to search for players
+while True:
+    key = input("Search player name: \n").lower()
+    # end prompter
+    if key == "q":
+        break
+
+    c = 0
+    # find and print results, if any found
+    for player in data:
+        if key in player['tag'].lower() or key in player['tag2'].lower():
+            rat = player['ovr'] + '/' + player['pot']
+            exp = ' (exp: ' + player['end'] + ')'
+            print(player['name'] + ', ' + player['age'] + ', ' + rat + ', ' + player['team'] + exp)
+            c += 1
+
+    if c == 0:
+        print("No results found")
+
+
+
+
+
--- soFifaScraper.py
+++ soFifaScraper.py
@@ -1,8 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as bs
 import json
+import unidecode
 
 
+# return json with SoFifa player data
 def get_player_data():
     # request page and set up parser
     url = 'https://sofifa.com/players?type=all&ptl=90'
@@ -12,17 +14,24 @@ def get_player_data():
 
     # get player stats within page
     data = []
-    c = 0
     for row in rows:
         d = dict()
+        # store player name and searchable names
         d['name'] = row.select_one('.col-name .tooltip')['data-tooltip'].strip()
+        d['tag'] = unidecode.unidecode(d['name'])
+        d['tag2'] = unidecode.unidecode(row.select_one('.col-name .tooltip .bp3-text-overflow-ellipsis').text.strip())
+
+        # store age, overall rating, potential rating
         d['age'] = row.select_one('.col-ae').text.strip()
         d['ovr'] = row.select_one('.col-oa').text.strip()
         d['pot'] = row.select_one('.col-pt').text.strip()
+
+        # store team and contract end date
         d['team'] = row.select('.col-name')[1].text.strip()
         d['team'] = d['team'].split('\n')[0]
         d['end'] = row.select_one('.col-name .sub').text.strip()
         d['end'] = d['end'].split(' ')[-1]
+
         data.append(d)
 
     # store data in json file
